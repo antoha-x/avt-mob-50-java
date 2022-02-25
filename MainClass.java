19377ce79b46157214266d7e2ef91691aa0731f9
Antoha-x <272acy@gmail.com>
2022-02-25 23:40:42 +0500

    Test 2 complete

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,11 @@
 public class MainClass {
-    public static int getLocalNumber() {
+    private static final int class_number = 20;
+
+    public int getClassNumber() {
+        return class_number;
+    }
+
+    public int getLocalNumber() {
         return 14;
     }
 }
--- MainClassTest.java
+++ MainClassTest.java
@@ -2,10 +2,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
-    private static final int expectedValue = 14;
+    private static final int expectedValueTest1 = 14;
+    private static final int expectedValueTest2 = 45;
+
+    MainClass mc = new MainClass();
 
     @Test
     public void testGetLocalNumber() {
-        Assert.assertEquals("getLocalNumber return not 14", MainClass.getLocalNumber(), expectedValue);
+        Assert.assertEquals("getLocalNumber return not 14", mc.getLocalNumber(), expectedValueTest1);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("getClassNumber return less than 45",
+                    mc.getClassNumber() > expectedValueTest2);
     }
 }
