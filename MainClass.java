17a8607b1bca1361fc103ff447520dbdb0960e96
Antoha-x <272acy@gmail.com>
2022-02-26 01:12:35 +0500

    Test 3 complete

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,6 @@
 public class MainClass {
     private static final int class_number = 20;
+    private static final String class_string = "hello, world";
 
     public int getClassNumber() {
         return class_number;
@@ -7,5 +8,9 @@ public class MainClass {
 
     public int getLocalNumber() {
         return 14;
+    }
+
+    public String getClassString() {
+        return class_string;
     }
 }
--- MainClassTest.java
+++ MainClassTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 public class MainClassTest {
     private static final int expectedValueTest1 = 14;
     private static final int expectedValueTest2 = 45;
+    private static final String expectedValueTest3 = "%sello";
+    private static final String firstLetterTest3 = "h";
 
     MainClass mc = new MainClass();
 
@@ -16,5 +18,17 @@ public class MainClassTest {
     public void testGetClassNumber() {
         Assert.assertTrue("getClassNumber return less than 45",
                     mc.getClassNumber() > expectedValueTest2);
+    }
+
+    @Test
+    public void testGetClassString() {
+        String errorMessage = "getClassString does not contain the substring '" +
+                String.format(expectedValueTest3, firstLetterTest3.toUpperCase()) +
+                "' or '" +
+                String.format(expectedValueTest3, firstLetterTest3) +
+                "'";
+        Assert.assertTrue(errorMessage,
+                mc.getClassString().contains(String.format(expectedValueTest3, firstLetterTest3.toUpperCase())) ||
+                        mc.getClassString().contains(String.format(expectedValueTest3, firstLetterTest3)));
     }
 }
